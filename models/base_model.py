acd12260db7135f9f56b7f3eff9d3d61b74e9eb2
Albert Mpepo <albertmpepo@gmail.com>
2023-10-10 14:16:29 +0200

    3. BaseModel completed

--- models/base_model.py
+++ models/base_model.py
@@ -5,9 +5,10 @@ from datetime import datetime
 
 
 class BaseModel():
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = created_at
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         class_name = self.__class__.__name__
